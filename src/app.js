c6b68cb963d23a9b10a1cb9a929ec8613cb67fa7
geritwo <geritwo@gmail.com>
2017-01-20 12:27:17 +0100

    Controller and views set up based on CatNinja example. Angular-route installed as dependency. ISSUES: Controllers won'r work, CSS not working.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,36 @@
 const angular = require('angular');
+const ngRoute = require('angular-route');
 
-const app = angular.module('app', []);
+const app = angular.module('app', ['ngRoute']);
 
+app.config(['$routeProvider', function routeProvider($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: './views/login.html',
+      controller: 'LoginController',
+    })
+    .when('/list', {
+      templateUrl: './views/list.html',
+    })
+    .otherwise({
+      redirectTo: '/home',
+    });
+}]);
+
+app.contoller('LoginController', ['$scope', function controller($scope) {
+
+  $scope.userLogin = function userLogin() {
+    $scope.userLog.push(
+      {
+        email: $scope.user.email,
+        password: $scope.user.password
+      }
+    );
+    console.log($scope.userLog); // NOTE: 4debug;
+  };
+
+  $scope.userLog = [];
+
+}]);
 
 module.exports = app;
