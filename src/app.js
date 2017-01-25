395bab55ab50f4147c9263a8138b8263a45951e1
tamasc <tamas.czerjak@gmail.com>
2017-01-25 15:30:14 +0100

    small changes in order to show the response data in the home view

--- src/app.js
+++ src/app.js
@@ -3,23 +3,16 @@ const ngRoute = require('angular-route');
 
 const app = angular.module('app', ['ngRoute']);
 
-var links = [
-  {
-    "title":"Index.hu",
-    "url":"http://index.hu"
-  },
-  {
-    "title":"Szanalmas.hu",
-    "url":"http://szanalmas.hu"
-  }
-];
-
+var links = [];
 
 app.config(['$routeProvider', function routeProvider($routeProvider) {
   $routeProvider
   .when('/login', {
     templateUrl: './views/login.html',
     controller: 'LoginController'
+  })
+  .when('/register', {
+    templateUrl: './views/register.html',
   })
   .when('/home', {
     templateUrl: './views/list.html',
@@ -36,11 +29,11 @@ app.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
         email: $scope.user.email,
         password: $scope.user.password
     };
-    console.log($scope.userLog); // NOTE: 4debug;
     $http
       .post('/api/login', $scope.userLog)
       .then(function (response) {
-        console.log(response);
+        console.log(response.data.links);
+        links = response.data.links;
       });
   };
 }]);
