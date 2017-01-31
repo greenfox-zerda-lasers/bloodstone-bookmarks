92de592c838f91601b7adcdd62e9faf581598dc0
tamasc <tamas.czerjak@gmail.com>
2017-01-31 23:42:34 +0100

    set db connection configuration

--- db.js
+++ db.js
@@ -2,21 +2,13 @@
 
 const pg = require('pg');
 
-console.log(process.env.PGUSER, process.env.PGDATABASE);
-
 // create a config to configure both pooling behavior
 // and client options
 // note: all config is optional and the environment variables
 // will be read if the config is not present
-const config = {
-  user: 'postgres', //env var: PGUSER
-  database: 'bloodstone', //env var: PGDATABASE
-  password: 'alma', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
+
+
+const config = process.env.DATABASE_URL || "pg://postgres:alma@localhost:5432/bloodstone";
 
 // instantiate a new client
 // the client will read connection information from
@@ -45,4 +37,4 @@ const queryDb = function (queryText, callback) {
 };
 
 
-queryDb('SELECT * FROM users', console.log;
+queryDb('SELECT * FROM users', console.log);
