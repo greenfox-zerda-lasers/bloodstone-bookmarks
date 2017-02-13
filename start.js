7d4969fe8b5b4e2e0f51186b3ec53c01bc304219
geritwo <geritwo@gmail.com>
2017-02-13 14:32:54 +0100

    Server files put in separate folder

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const db = require('./conf/db/db');
-const server = require('./server.js');
+const db = require('./server/db/db');
+const server = require('./server/server.js');
 
 const myServer = server(db);
 
