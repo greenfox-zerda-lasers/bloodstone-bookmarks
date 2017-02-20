fb8fd1d5547d6a77735ed76b92cd6186024f9843
tamasc <tamas.czerjak@gmail.com>
2017-02-20 15:33:08 +0100

    fix registration once again

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ angular.module('app').config(['$routeProvider', function routeProvider($routePro
   })
   .when('/register', {
     templateUrl: './views/register.html',
-    controller: 'RegistrationController as register'
+    controller: 'RegistrationController as registration'
   })
   .when('/home', {
     templateUrl: './views/bookmarks.html',
