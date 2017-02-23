fcb2071a5d94c73fb7c24360289320c1027b03ec
geritwo <geritwo@gmail.com>
2017-02-23 21:56:08 +0100

    Footer url corrected

--- server/bookmarks.js
+++ server/bookmarks.js
@@ -1,9 +1,9 @@
 const bookmarks = function bookmarks(queryFunction) {
 
   const saveBookmark = (userID, url, title, success) => {
-    // ID, user id, url, title
-    // usr ID?
-    queryFunction(`INSERT INTO bookmarks (user_id, url, title) VALUES ('${userID}', '${url}', '${title}') RETURNING (url)`, success);
+    return new Promise(function (resolve, reject){
+      queryFunction(`INSERT INTO bookmarks (user_id, url, title) VALUES ('${userID}', '${url}', '${title}') RETURNING (url)`, success);
+    })
   };
 
   const getList = (userID, success) => {
--- test-frontend/frontend-tests.js
+++ test-frontend/frontend-tests.js
@@ -1,5 +1,16 @@
+'use strict';
+
 describe('Testing the test', function () {
   it('has a dummy spec to test 2 + 2', function () {
     expect(2 + 2).toEqual(4);
   });
 });
+
+// USER HANDLING
+
+describe('Login controller', function() {
+  var LoginController;
+  var
+});
+
+// BOOKMARK MANAGEMENT
